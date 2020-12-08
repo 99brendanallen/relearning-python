74daac6eabd3aee98b1bf438df85aaa561661b1d
Brendan Allen <99brendanallen@gmail.com>
2020-12-08 13:15:02 -0500

    Type Conversion

---
+++ typeconversion.py
@@ -0,0 +1,5 @@
+birth_year = int(input('Birth Year: '))
+#print(type(birth_year))
+age = 2020 - birth_year
+#print(type(age))
+print(age)
---
+++ typeconversion_exercise.py
@@ -0,0 +1,3 @@
+lbs = int(input("What is your weight? "))
+kg = lbs * 0.45
+print(kg)
