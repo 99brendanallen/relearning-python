c177322d657b3cecef0d97b58d4ca20b6e158ddd
Brendan Allen <99brendanallen@gmail.com>
2020-12-08 12:28:22 -0500

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,8 @@
+price = 10
+price = 20 #python will always use the latest one
+
+rating = 4.9
+name = 'Brendan'
+is_published = False #boolean, use capital letters at the beginning
+###print (price)
+
